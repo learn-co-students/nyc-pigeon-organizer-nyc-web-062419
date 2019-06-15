8f31e4f307ee5c5873b2ff5d960ce72612298982
Luis Ascencio <luismiguelascencio@gmail.com>
2019-06-15 15:45:15 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
+# frozen_string_literal: true
+
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  data.each_with_object({}) do |(keys, attributes), pigeon_list|
+    attributes.each do |value, pigeon_names|
+      pigeon_names.each do |name|
+        pigeon_list[name] ||= {}
+        binding.pry
+        pigeon_list[name][keys] ||= []
+        binding.pry
+        pigeon_list[name][keys].push(value.to_s)
+        binding.pry
+      end
+    end
+  end
 end
