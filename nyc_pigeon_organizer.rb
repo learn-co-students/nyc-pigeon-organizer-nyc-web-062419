7d549e238db7e4c388f54be84f3395b4c130207a
IanGrubb <yangrubb@gmail.com>
2019-06-19 23:17:56 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+
+  pigeon_hash = {}
+
+  data.each do |property_type, property_hash|
+    property_hash.each do |property, bearers|
+      bearers.each do |bearer|
+        if pigeon_hash.keys.include? bearer
+          if pigeon_hash[bearer].keys.include? property_type
+            pigeon_hash[bearer][property_type].push(property.to_s)
+          else
+            pigeon_hash[bearer][property_type] = [property.to_s]
+          end
+        else
+          pigeon_hash[bearer] = {property_type => [property.to_s]}
+        end
+      end
+    end
+  end
+
+  pigeon_hash
 end
