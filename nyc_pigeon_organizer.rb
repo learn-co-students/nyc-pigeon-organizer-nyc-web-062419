184b8fc8232195a6b9b02cdd854f7b3328756998
Anthony Wan <Anthonywan00@gmail.com>
2019-06-30 15:35:36 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
+require "pry"
 def nyc_pigeon_organizer(data)
   # write your code here!
+  x = {}
+  data.each do |info,option|
+    option.each do |option,name|
+      name.each do |pigeon|
+        x[pigeon] ||= {}
+        x[pigeon][info] ||= []
+        x[pigeon][info] << option.to_s
+      end
+    end
+  end
+  x
 end
+
